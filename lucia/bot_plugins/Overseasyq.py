3cbf28fdfe6f4b38af55dd45eaed1185309d2687
longchengguxiao <75836227+longchengguxiao@users.noreply.github.com>
2021-01-26 12:34:32 +0800

    Update Overseasyq.py

--- lucia/bot_plugins/Overseasyq.py
+++ lucia/bot_plugins/Overseasyq.py
@@ -14,7 +14,7 @@ import json
 #soup = BeautifulSoup(html,'lxml')
 
 async def get_overseas_yq():
-    url = 'https://api.inews.qq.com/newsqa/v1/automation/foreign/country/ranklist'
+
     #headers = {
     #    'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0',
     #    'referer':'https://news.qq.com/zt2020/page/feiyan.htm'
