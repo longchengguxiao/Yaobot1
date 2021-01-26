92878b7eeb6aefa9a0a7dd156cdeadc3926630e0
longchengguxiao <75836227+longchengguxiao@users.noreply.github.com>
2021-01-26 12:31:15 +0800

    Update bot_config.py

--- lucia/bot_config.py
+++ lucia/bot_config.py
@@ -19,11 +19,11 @@ SESSION_CANCEL_EXPRESSION = (
     '那lucia先不打扰大家啦',
 )
 # 机器人昵称，设定后 "@机器人 天气" 和 "lucia 天气" 效果相同。
-NICKNAME = { 'lucia', '垚', '小小垚' }
+NICKNAME = { }
 # 关闭调试输出，提升性能。
 DEBUG = False
 # 表示“超级用户”，也就是机器人的主人。超级用户拥有最高的权限。在这里填入你的 QQ 号。
-SUPERUSERS = { 1298919732 , 279764456}
+SUPERUSERS = { }
 # 表示一条命令的超时（没有用户输入）时间。
 SESSION_RUN_TIMEOUT = timedelta(seconds=10)
 # 服务器和端口
